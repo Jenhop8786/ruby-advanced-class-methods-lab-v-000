07c0adee0473d66e872d2abaf407e57c032ae146
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-16 00:39:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,6 +29,10 @@ def self.create_by_name(song_name)
 end
 
 def self.find_by_name(song_name)
+  self.all.detect {|song| song.name == song_name}
+end 
+
+def self.find_or_create_by_name(song_name)
   self.find_by_name(song_name) ||
   self.create_by_name(song_name)
 end
