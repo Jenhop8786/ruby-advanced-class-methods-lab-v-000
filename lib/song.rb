82bfbda292a65a6d1ef43188080e7a3aa3bd9c96
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2017-12-16 00:31:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,41 +11,39 @@ class Song
   end
 
 def self.create
-  song = Song.new 
-  song.save 
-  song 
+  song = Song.new
+  song.save
+  song
 end
 
 def self.new_by_name(song_name)
-  song = self.new 
+  song = self.new
   song.name = song_name
-  song 
-end 
+  song
+end
 
 def self.create_by_name(song_name)
-  song = self.create 
+  song = self.create
   song.name = song_name
-  song 
-end 
+  song
+end
 
 def self.find_or_create_by_name(song_name)
   self.find_by_name(song_name) ||
   self.create_by_name(song_name)
-end 
+end
 
 def self.alphabetical
   self.all.sort_by {|song| song.name}
-end 
+end
 
 def self.new_from_filename(file)
   file = file.split(" - ")
   artist_name = file[0]
-  song_name = file[1].gsub(".mp3, "")
+  song_name = file[1].gsub(".mp3", "")
 
-  song = self.new 
+  song = self.new
   song.name = song_name
   song.artist_name = artist_name
-  song 
-end 
-
-
+  song
+end
